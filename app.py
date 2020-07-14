560c5085603424161eb67550e0d17b457d99e870
jamiejamiebobamie <jmccrory@vt.edu>
2020-07-14 03:04:00 -0700

    added a /generate route and added more cached quotes. need to launch heroku site and add databse.

--- app.py
+++ app.py
@@ -4,16 +4,26 @@ from src.tweet import get_tweet, pick_random_from_array
 
 @app.route('/')
 def _main():
-    # file = 'public/data/tokenized_Shakespeare.md'
-    # tweet = get_tweet(file)
     file = 'src/quotes_tokenized_Shakespeare.md'
     tweets = []
     with open(file, "r") as pregenerated_tweets:
         for line in pregenerated_tweets:
             tweets.append(line)
     tweet = pick_random_from_array(tweets)
-
     return render_template('index.html', title='Home',tweet=tweet)
+
+# testing. will crud to db.
+@app.route('/generate')
+def generate():
+    read_filepath =  "public/data/tokenized_Shakespeare.md"
+    filepath = read_filepath.split("/")
+    write_filename = filepath[-1]
+    relative_path = "/".join(filepath[:-1])
+    write_filepath = "./src/quotes_"+write_filename
+    quote = get_tweet(read_filepath)
+    with open(write_filepath, "a") as tweets:
+        tweets.write(quote + "\n")
+    return render_template('index.html', title='Home',tweet=quote)
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 3000)
