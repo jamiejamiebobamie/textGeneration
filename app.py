a603e94fe13c50d1ce9caa62f9938b66eb3e985d
jamiejamiebobamie <jmccrory@vt.edu>
2020-08-02 15:32:58 -0700

    testing

--- app.py
+++ app.py
@@ -357,7 +357,7 @@ def serve_quote_from_twitter():
               if hasattr(status, "text"):
                 text = status.text
                 tweet_content.append(text)
-              if tweet_count > 1000:
+              if tweet_count > 1:
                   break
 
             # words_from_tweets = []
